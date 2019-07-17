3b93a78e346746c2733f83def6bfabf4ca101688
Robert0306 <robert.kaino@gmail.com>
2019-07-17 02:15:12 +0200

    Started to build the blogpost model

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Simple flask restfull blog-Api
 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 ## About
 This blog-api is a learning project for using flask as backend and for making a RESTfull api with JWT authentication.<br>
 It will also serve as a part of my personal webpage
--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, text
+import datetime
 
 
 Base = declarative_base()
 
-
+# The user model is kept basic.
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
@@ -12,3 +13,15 @@ class User(Base):
     email = Column(String(80), unique=True, nullable=False)
     password = Column(String(80))
     admin = Column(Boolean)
+
+# The blogpost model is simple, but contains ForeignKey for relations to users
+# Should also contain a relation to comment feature... 
+
+class Blogpost(Base):
+    __tablename__ = "blogpost"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    content = Column(text, nullable=False)
+    author_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    created_at = Column(Datetime)
+    modified_at = Column(datetime)
