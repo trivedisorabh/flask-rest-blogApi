51e2d716f4a40fea531e3331504b874752deaa60
Robert0306 <robert.kaino@gmail.com>
2019-07-31 01:04:32 +0200

    Config variables set from environment

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 #configuration file for the flask app.
 #add new configurations here
+
+import os
+
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://robert:moss@localhost/blog_api_db'
-    SECRET_KEY = 'thisissecret'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
 # DATABASE_URI = 'postgresql://robert:moss@localhost/blog_api_db'
